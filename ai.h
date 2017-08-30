219a96651961ee7aabbc32e50edc832477069472
xahigoyez <pierre.bertin-johannet@orange.fr>
2017-08-31 00:04:47 +0200

    added config read

---
+++ ai.h
@@ -0,0 +1,8 @@
+//
+// Created by pierre on 30/08/17.
+//
+
+#ifndef STICKWAR_AI_H
+#define STICKWAR_AI_H
+
+#endif //STICKWAR_AI_H
---
+++ loader.h
@@ -0,0 +1,8 @@
+//
+// Created by pierre on 29/08/17.
+//
+
+#ifndef STICKWAR_LOADER_H
+#define STICKWAR_LOADER_H
+
+#endif //STICKWAR_LOADER_H
