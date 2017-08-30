19776622760a0367aaf30fc27b2ff6c75fa252d1
xahigoyez <xahigoyez@gmail.com>
2017-08-30 13:08:19 +0000

    utils.h edited online with Bitbucket

--- utils.h
+++ utils.h
@@ -9,4 +9,7 @@
 char* string_concat(char* a, char* b);
 
 
+json_t* start_json(char* source);
+json_t* read_elem(json_t* parent, char* elem, char* message, json_type desired);
+json_t* read_index(json_t* parent, int index, char* message, json_type desired);
 #endif /* UTILS_H */
