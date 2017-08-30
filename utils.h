3fcbc6f5ae8c148858179bf6cacc0cf324f473f0
xahigoyez <xahigoyez@gmail.com>
2017-08-30 10:15:14 +0000

    utils.h created

---
+++ utils.h
@@ -0,0 +1,12 @@
+//
+// Created by Pierre on 30/08/2017
+// This file contains a few functions on strings and stuffs.
+//
+
+#ifndef UTILS_H
+#define UTILS_H
+
+char* string_concat(char* a, char* b);
+
+
+#endif /* UTILS_H */
